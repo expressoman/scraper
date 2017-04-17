fa3bc15c99d827bb04d60a149da650fc9e502fa2
moris <moris.zhang@gmail.com>
2017-04-17 20:24:07 +0800

    add log rotate

--- log.go
+++ log.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	log "github.com/Sirupsen/logrus"
+	"gopkg.in/natefinch/lumberjack.v2"
+
 	"io"
 	"fmt"
 	"os"
-	"time"
+	_ "time"
     "syscall"
     "os/signal"
 
+)
+
+const(
+	filename ="scraper.log"
+	rotate = 10
 )
 
 type logBot struct{
 	file *os.File
 	ch chan os.Signal
 }
+
 func newLogBot() *logBot{
 	// Log as JSON instead of the default ASCII formatter.
 	//log.SetFormatter(&log.JSONFormatter{})
@@ -22,12 +30,19 @@ func newLogBot() *logBot{
 
 	// Output to stdout instead of the default stderr
 	// Can be any io.Writer, see below for File example
-	log.SetOutput(os.Stdout)
+	//log.SetOutput(os.Stdout)
 
 	lb := new(logBot)
+	log.SetOutput(io.MultiWriter(&lumberjack.Logger{
+	    Filename:   filename,
+	    MaxSize:    30, // megabytes
+	    MaxBackups: 30,
+	    MaxAge:     28, //days
+	},os.Stdout))
 
+/*
 	var err error
-	lb.file, err = os.OpenFile("scraper.log", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0666)
+	lb.file, err = os.OpenFile(fno, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0666)
 	if err == nil {
 		//	log.SetOutput(file)
 		log.SetOutput(io.MultiWriter(lb.file, os.Stdout))
@@ -35,7 +50,7 @@ func newLogBot() *logBot{
 	} else {
 		log.Fatal("Failed to log to file, using default stderr")
 	}
-
+*/
 // Only log the warning severity or above.
 	fmt.Printf(*debug_level)
 	var l log.Level
@@ -55,12 +70,14 @@ func newLogBot() *logBot{
 	lb.ch = make( chan os.Signal)
 //	signal.Notify(lb.ch, syscall.SIGUSR1,syscall.SIGINT,syscall.SIGTERM)
 	signal.Notify(lb.ch, syscall.SIGUSR1)
-
+/*
 	go func (){
 		s:=<-lb.ch
-		log.Printf("signal : %v",s)
+		fmt.Printf("signal : %v",s)
 		lb.reset()
 	}()
+*/
+
 	return lb
 }
 
@@ -68,12 +85,11 @@ func (lb *logBot) close(){
 	lb.file.Close()
 }
 
+
 func (lb *logBot) reset(){
 	lb.file.Close()
-	time.Sleep(1200*time.Millisecond)
-
 	var err error
-	lb.file, err = os.OpenFile("scraper.log", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0666)
+	lb.file, err = os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0666)
 	if err == nil {
 		//	log.SetOutput(file)
 		log.SetOutput(io.MultiWriter(lb.file, os.Stdout))
@@ -84,4 +100,9 @@ func (lb *logBot) reset(){
 }
 
 
+
+func (lb *logBot) Debug(){
+	log.Debug("tet")
+
+}
 
